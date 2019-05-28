dc13d917b95e8d70f5ec96a4f03c790970f1b5e6
unknown <belathne0429@gmail.com>
2019-05-28 17:55:09 +0900

    [#1] Model 생성
    - user 모델 생성
    
    ING #1

---
+++ src/com/model/User.java
@@ -0,0 +1,34 @@
+package com.model;
+
+public class User {
+	private String id;
+	private String pw;
+	private String spec;
+	private String tel;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getSpec() {
+		return spec;
+	}
+	public void setSpec(String spec) {
+		this.spec = spec;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+}
