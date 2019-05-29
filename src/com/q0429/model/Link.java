b260e19f528937dd35286fae2df3c92c54dc3031
unknown <belathne0429@gmail.com>
2019-05-29 13:38:42 +0900

    [#1] Model 추가
    - 누락된 Link파일 추가
    
    Done #1

---
+++ src/com/q0429/model/Link.java
@@ -0,0 +1,33 @@
+package com.q0429.model;
+
+public class Link {
+	private int link_num;
+	private int pj_num;
+	private String require;
+	private String id;
+	
+	public int getLink_num() {
+		return link_num;
+	}
+	public void setLink_num(int link_num) {
+		this.link_num = link_num;
+	}
+	public int getPj_num() {
+		return pj_num;
+	}
+	public void setPj_num(int pj_num) {
+		this.pj_num = pj_num;
+	}
+	public String getRequire() {
+		return require;
+	}
+	public void setRequire(String require) {
+		this.require = require;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}	
+}
